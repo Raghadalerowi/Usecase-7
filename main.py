036f3cbef4c33e053b42e85e54ec3e9697906cb3
Rh3al <rudyal3rowi@gmail.com>
2024-11-17 22:18:49 +0300

    Raghad Alerowi USECASE 7c

--- main.py
+++ main.py
@@ -14,26 +14,28 @@ def root():
 
 class InputFeatures(BaseModel):
     age: float
+    height:float
     appearance: int
     minutes_played: float
-    days_injured: int
-    games_injured: int
-    award: int
     highest_value: float
+    current_value:float
     position: str
 
 
 def preprocessing(input_features: InputFeatures):
     dict_f = {
         'age': input_features.age,
+        'height': input_features.height,
         'appearance': input_features.appearance,
         'minutes played': input_features.minutes_played,
-        'days_injured': input_features.days_injured,
-        'games_injured': input_features.games_injured,
-        'award': input_features.award,
         'highest_value': input_features.highest_value,
+        'current_value': input_features.current_value,
         'position_Goalkeeper': int(input_features.position == 'Goalkeeper'),
         'position_midfield': int(input_features.position == 'Midfield'),
+        'position_Attack': int(input_features.position == 'Attack'),
+        'position_Defender': int(input_features.position == 'Defender'),
+
+
     }
     print(f"dict_f: {dict_f}")  # Add debug log here
     features_list = [dict_f[key] for key in sorted(dict_f)]
--- streamlit.py
+++ streamlit.py
@@ -22,23 +22,21 @@ st.title("Football Player Prediction")
 
 # Input fields for the prediction
 age = st.number_input("Age", min_value=18, max_value=40, value=25)
+height = st.number_input("Height (CM)", min_value=150, max_value=200, value=150)
 appearance = st.number_input("Appearances", min_value=0, value=30)
 minutes_played = st.number_input("Minutes Played", min_value=0, value=2700)
-days_injured = st.number_input("Days Injured", min_value=0, value=15)
-games_injured = st.number_input("Games Injured", min_value=0, value=3)
-award = st.number_input("Award (Numeric)", min_value=0, value=1)
-highest_value = st.number_input("Highest Value", min_value=0.0, value=15.5)
-position = st.selectbox("Position", options=["Goalkeeper", "Midfield"])
+highest_value = st.number_input("Highest Value", min_value=0.0, value=100)
+current_value = st.number_input("Current Value", min_value=0.0, value=100)
+position = st.selectbox("Position", options=["Goalkeeper", "Midfield","Attack","Defender"])
 
 # Prepare the data for the API call
 data = {
     "age": age,
+    "height":height,
     "appearance": appearance,
-    "minutes_played": minutes_played,
-    "days_injured": days_injured,
-    "games_injured": games_injured,
-    "award": award,  # Ensure the type matches API requirements
+    "minutes_played": minutes_played, # Ensure the type matches API requirements
     "highest_value": highest_value,
+    "current_value":current_value,
     "position": position
 }
 
